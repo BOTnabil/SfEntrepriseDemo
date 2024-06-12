0b9df3ce4c154aeb7cfdc09bfded297452de9c82
Nabil <l4teoyt@gmail.com>
2024-06-12 15:00:42 +0200

    affichage tableau entreprise

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -11,8 +11,11 @@ class EntrepriseController extends AbstractController
     #[Route('/entreprise', name: 'app_entreprise')]
     public function index(): Response
     {
+        $name = "Elan formation";
+        $tableau = ["valeur 1","valeur 2","valeur 3"];
         return $this->render('entreprise/index.html.twig', [
-            'controller_name' => 'EntrepriseController',
+            'controller_name' => $name,
+            'tableau' => $tableau
         ]);
     }
 }
